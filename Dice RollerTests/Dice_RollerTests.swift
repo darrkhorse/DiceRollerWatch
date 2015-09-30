da64db420e2be4ebb3ab4514b68b3a700a609766
Hinck, Johann A <f00312298@s114d-02-osx-3.local>
2015-09-29 23:24:52 -0500

    Initial Commit

---
+++ Dice RollerTests/Dice_RollerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dice_RollerTests.swift
+//  Dice RollerTests
+//
+//  Created by Hinck, Johann A on 9/29/15.
+//  Copyright © 2015 Hinck, Johann A. All rights reserved.
+//
+
+import XCTest
+@testable import Dice_Roller
+
+class Dice_RollerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
